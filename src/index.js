beee54bd825126c76bc052701c6bd9ee1a726069
Cihad Horuzoglu <cihad.horuzoglu@ciceksepeti.com>
2018-07-03 21:19:48 +0300

    step-02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
  import _ from 'lodash';
+ import './style.css';
+ import Icon from './icon.png';
 
  function component() {
-    var element = document.createElement('div');
+     var element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+     element.classList.add('hello');
 
-    return element;
-}
+     // Add the image to our existing div.
+     var myIcon = new Image(); 
+     myIcon.src = Icon; 
+     element.appendChild(myIcon);
 
-document.body.appendChild(component());
+     return element;
+ }
+
+ document.body.appendChild(component());
