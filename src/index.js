bd6dd7a3b121854ae47fc9dec25f5fb5c3dd4111
Cihad Horuzoglu <cihad.horuzoglu@trendyol.com>
2018-07-17 23:10:24 +0300

    icon changed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
  import Icon from './icon.png';
 
  function component() {
-     var element = document.createElement('div');
+     const element = document.createElement('div');
 
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
      element.classList.add('hello');
 
      // Add the image to our existing div.
-     var myIcon = new Image(); 
-     myIcon.src = Icon; 
+     const myIcon = new Image();
+     myIcon.src = Icon;
      element.appendChild(myIcon);
 
      return element;
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,15 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "dist/"
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-            }, 
+            },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
